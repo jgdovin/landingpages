09767e16f1883be75fd404ce6af52a4b9f4693fa
Joshua Gdovin <joshuag@focusschoolsoftware.com>
2015-07-27 14:12:39 -0400

    initial commit

---
+++ tests/cucumber/features/landing-page/curate_a_landing_page.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+
+  this.Given(/^I have created a landing page with the heading "([^"]*)"$/,
+    function (heading) {
+      return this.server.call('page/create', {path: '/', heading: heading});
+    });
+
+  this.When(/^a user navigates to the landing page$/, function () {
+    return this.client.url(process.env.ROOT_URL);
+  });
+
+  this.Then(/^they see the heading "([^"]*)"$/, function (heading) {
+    return this.client.
+      waitForExist('h1').
+      getText('h1').
+      should.become(heading);
+  });
+
+};
